ab57c0ae7fc613b2c22ed7de6cd56a767c604503
Phanindra <107109784+Phanindra11@users.noreply.github.com>
2022-06-09 20:32:50 +0530

    Add files via upload

---
+++ Java_Doc/HelloWorld/src/Caption.java
@@ -0,0 +1,9 @@
+
+public class Caption {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("'Ability is what you’re capable of doing. Motivation determines what you do. Attitude determines how well you do it.'");
+	}
+
+}
